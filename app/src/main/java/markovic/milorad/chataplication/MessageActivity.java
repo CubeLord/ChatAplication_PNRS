1a0127420a9651cddbc9b7b8589e1a6ee2209673
Cube.Lord <mmiloradmiso@gmail.com>
2018-03-19 19:37:21 +0100

    Added send button TextWatcher conditions

--- app/src/main/java/markovic/milorad/chataplication/MessageActivity.java
+++ app/src/main/java/markovic/milorad/chataplication/MessageActivity.java
@@ -3,13 +3,16 @@ package markovic.milorad.chataplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import static android.widget.Toast.*;
 
-public class MessageActivity extends AppCompatActivity implements View.OnClickListener {
+public class MessageActivity extends AppCompatActivity implements View.OnClickListener, TextWatcher {
 
     private static final String SENT_LOG_MESSAGE = "Message is sent!";
     Toast toast = null;
@@ -21,7 +24,9 @@ public class MessageActivity extends AppCompatActivity implements View.OnClickLi
 
         Button logout = (Button)findViewById(R.id.messageActButtonLogout);
         Button send = (Button)findViewById(R.id.messageActButtonSend);
+        EditText messageText = (EditText)findViewById(R.id.messageActEditMessageText);
 
+        messageText.addTextChangedListener(this);
         send.setOnClickListener(this);
         logout.setOnClickListener(this);
     }
@@ -39,6 +44,25 @@ public class MessageActivity extends AppCompatActivity implements View.OnClickLi
                 toast = Toast.makeText(this,SENT_LOG_MESSAGE, Toast.LENGTH_LONG);
                 toast.show();
                 break;
+        }
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        if(((EditText)findViewById(R.id.messageActEditMessageText)).getText().toString().equals("")){
+            ((Button)findViewById(R.id.messageActButtonSend)).setEnabled(false);
+        } else {
+            ((Button)findViewById(R.id.messageActButtonSend)).setEnabled(true);
         }
     }
 }
